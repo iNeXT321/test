aa34de00a89f5057006a3305e10522e1bc3aade1
iNeXT <upeng321@163.com>
2019-07-06 03:11:05 +0800

    git 测试 version 1.0

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String str = "11";
+        System.out.println(DecimaltoBinaryTo(str));
+    }
+
+    public static String DecimaltoBinaryTo(String str) {
+        int num = Integer.valueOf(str);
+        String strB = "";
+
+        while (num > 0) {
+            strB = num % 2 + strB;
+            num = num / 2;
+        }
+        return strB;
+
+
+    }
+}
+
+
